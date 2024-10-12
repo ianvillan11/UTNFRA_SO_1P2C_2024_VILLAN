15b3b708b0ce15b4d1927213a6c32d02352c200c
ianvillan11 <ianvillan4@gmail.com>
2024-10-12 19:49:35 +0000

    FEAT: Agregado el contenido del script_A

---
+++ Punto_A.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Crear la estructura de los examenes
+sudo mkdir -p /EXAMENES-UTN/Alumno_1/{parcial_1,parcial_2,parcial_3}
+sudo mkdir -p /EXAMENES-UTN/Alumno_2/{parcial_1,parcial_2,parcial_3}
+sudo mkdir -p /EXAMENES-UTN/Alumno_3/{parcial_1,parcial_2,parcial_3}
+sudo mkdir -p /EXAMENES-UTN/PROFESORES
